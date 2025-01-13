904f1d36b2d88e4c8f4ce85062d7edc22b9127e5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-13 11:11:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-@st.cache_resource
 def init_connection():
     url = st.secrets["SUPABASE_URL"]
     key = st.secrets["SUPABASE_KEY"]
