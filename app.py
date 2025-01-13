1d498446f7ce35be5f98b46e785e0b483ef21c91
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-13 10:08:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import streamlit as st
-from st_supabase_connection import SupabaseConnection
+from supabase import create_client, Client
 
 # Initialize connection.
-conn = st.connection("supabase",type=SupabaseConnection)
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
 
-# Perform query.
-rows = conn.query("*", table="df", ttl="10m").execute()
+supabase = init_connection()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
 def run_query():
-    return supabase.table("df").select("*").execute()
-    
-rows = run_query()
+    return supabase.table("mytable").select("*").execute()
 
+rows = run_query()
+    
 # Print results.
 for row in rows.data:
     st.write(f"{row['waarnemer']} has a :{row['soort']} found ({row['datum']}):")
